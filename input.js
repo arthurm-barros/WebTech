f98f0a55c4851c0f5dcdfeed33c6fb51bd7ff092
Cauã Vitor <vitor.caua@escolar.ifrn.edu.br>
2026-02-01 16:46:22 -0300

    Fix: animações relativas

--- input.js
+++ input.js
@@ -1,11 +1,10 @@
 const Add = document.getElementById('add')
 const form = document.getElementById('form-modal')
-const fechado = document.getElementById('saidera')
 Add.addEventListener('click', (event)=> {
     event.preventDefault()
     form.style.display = 'grid'
 })
-fechado.addEventListener('click', (event) => {
+form.addEventListener('click', (event) => {
     event.preventDefault()
     if (event.target === form){
     form.style.display = 'none'
